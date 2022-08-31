5b2cea4b07d0d0f8575b9d456b031932e5a8dade
Nhan Nguyen <nhantnguyen42@gmail.com>
2022-08-31 10:19:53 -0700

    Added searchbar and styling.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,14 @@ const WeatherManager = require("./weathermanager.js");
 const Init = require("./init.js");
 wm = WeatherManager();
 document.body.appendChild(Init());
+
+const search = document.querySelector(".search");
+const input = document.querySelector(".input");
+search.addEventListener("submit", (event) => {
+  event.preventDefault();
+  wm.setLocation(input.value)
+  wm.getWeatherData().then(function(e) {
+    console.log(wm.getTemperature());
+  });
+  input.value = "";
+});
